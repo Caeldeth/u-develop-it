3308671ff97413304b1f3264471e1e5c5b4fb4af
Caeldeth <sabrael@hybrasyl.com>
2021-11-13 19:28:43 -0600

    connect to db

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.port || 3001;
 const app = express();
@@ -5,6 +6,17 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+// COnnect to database
+const db = mysql.createConnection(
+    {
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'Angiris324!#',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 app.use((req,res) => {
     res.status(404).end();
