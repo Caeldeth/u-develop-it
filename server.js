b59242a20d85644cdae05340ea1b5490b328dd83
Caeldeth <sabrael@hybrasyl.com>
2021-11-13 19:28:43 -0600

    connect to db

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.port || 3001;
 const app = express();
+var settings = require('./settings/settings.js');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+// COnnect to database
+const db = mysql.createConnection(
+    {
+        host: '127.0.0.1',
+        user: `${settings.username}`,
+        password: `${settings.password}`,
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 app.use((req,res) => {
     res.status(404).end();
---
+++ settings/example_settings.js
@@ -0,0 +1,6 @@
+//To use, copy this file and rename to settings.js
+
+//insert your MYSQL username here
+exports.username = 'username';
+//insert your MYSQL password here
+exports.password = 'password';
